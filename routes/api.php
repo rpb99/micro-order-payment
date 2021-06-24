48e4099d16b8dfc7651d602c3fe1d89dcf1e0fe1
rpb99 <rikipebrianto@gmail.com>
2021-06-24 10:44:46 +0700

    Initial commit

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\WebhookController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::post('orders', [OrderController::class, 'create']);
+Route::get('orders', [OrderController::class, 'index']);
+
+
+Route::post('webhook', [WebhookController::class, 'midtransHandler']);
